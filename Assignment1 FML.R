5c98de8cb860bbcebe1b64520e7e78bdcf6813ea
SrinivasMadhala <144407235+SrinivasMadhala@users.noreply.github.com>
2023-09-09 22:10:55 -0400

    Add files via upload
    I Have taken dataset from web and run the code about descriptive statistics , transformation of variables , Plot 
    from the dataset I have taken range from 2009-2013 to create plot

---
+++ Assignment1 FML.R
@@ -0,0 +1,14 @@
+#Importing Data set
+World_GDP_Dataset <- read_csv("C:/Users/msrin/Downloads/World GDP Dataset.csv")
+World_GDP_Dataset
+
+#descriptive statastics
+summary(World_GDP_Dataset[,c('GDP, current prices (Billions of U.S. dollars)','2001')])
+
+#Transformation of variable
+
+df <- World_GDP_Dataset
+df$log_2012 <- log10(df$'2012')
+df$log_2012
+#Plot
+plot(df$'2009' ,df$'2013' )
